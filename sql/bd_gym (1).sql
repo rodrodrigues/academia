3ef2b3b4eddc86a69f948cc622bc4f33c057e944
Rodrigo Rodrigues <rodsr98@gmail.com>
2018-06-14 19:21:17 -0300

    bd novo

--- sql/bd_gym (1).sql
+++ sql/bd_gym (1).sql
@@ -2,10 +2,10 @@
 -- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Tempo de geração: 14/06/2018 às 05:54
+-- Host: 127.0.0.1
+-- Generation Time: 15-Jun-2018 às 00:20
 -- Versão do servidor: 10.1.32-MariaDB
--- Versão do PHP: 7.2.5
+-- PHP Version: 7.2.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `bd_gym`
+-- Database: `bd_gym`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `avaliacao`
+-- Estrutura da tabela `avaliacao`
 --
 
 CREATE TABLE `avaliacao` (
@@ -40,7 +40,7 @@ CREATE TABLE `avaliacao` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `funcionario`
+-- Estrutura da tabela `funcionario`
 --
 
 CREATE TABLE `funcionario` (
@@ -52,7 +52,7 @@ CREATE TABLE `funcionario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Fazendo dump de dados para tabela `funcionario`
+-- Extraindo dados da tabela `funcionario`
 --
 
 INSERT INTO `funcionario` (`matricula`, `nome`, `ocupacao`, `email`, `senha`) VALUES
@@ -61,7 +61,61 @@ INSERT INTO `funcionario` (`matricula`, `nome`, `ocupacao`, `email`, `senha`) VA
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `treino`
+-- Estrutura da tabela `serie`
+--
+
+CREATE TABLE `serie` (
+  `mat_usuario` int(11) DEFAULT NULL,
+  `exercicio1` varchar(100) DEFAULT NULL,
+  `exercicio2` varchar(100) DEFAULT NULL,
+  `exercicio3` varchar(100) DEFAULT NULL,
+  `exercicio4` varchar(100) DEFAULT NULL,
+  `exercicio5` varchar(100) DEFAULT NULL,
+  `exercicio6` varchar(100) DEFAULT NULL,
+  `exercicio7` varchar(100) DEFAULT NULL,
+  `exercicio8` varchar(100) DEFAULT NULL,
+  `exercicio9` varchar(100) DEFAULT NULL,
+  `exercicio10` varchar(100) DEFAULT NULL,
+  `exercicio11` varchar(100) DEFAULT NULL,
+  `exercicio12` varchar(100) DEFAULT NULL,
+  `repeticao1` varchar(100) DEFAULT NULL,
+  `repeticao2` varchar(100) DEFAULT NULL,
+  `repeticao3` varchar(100) DEFAULT NULL,
+  `repeticao4` varchar(100) DEFAULT NULL,
+  `repeticao5` varchar(100) DEFAULT NULL,
+  `repeticao6` varchar(100) DEFAULT NULL,
+  `repeticao7` varchar(100) DEFAULT NULL,
+  `repeticao8` varchar(100) DEFAULT NULL,
+  `repeticao9` varchar(100) DEFAULT NULL,
+  `repeticao10` varchar(100) DEFAULT NULL,
+  `repeticao11` varchar(100) DEFAULT NULL,
+  `repeticao12` varchar(100) DEFAULT NULL,
+  `quantidade1` varchar(100) DEFAULT NULL,
+  `quantidade2` varchar(100) DEFAULT NULL,
+  `quantidade3` varchar(100) DEFAULT NULL,
+  `quantidade4` varchar(100) DEFAULT NULL,
+  `quantidade5` varchar(100) DEFAULT NULL,
+  `quantidade6` varchar(100) DEFAULT NULL,
+  `quantidade7` varchar(100) DEFAULT NULL,
+  `quantidade8` varchar(100) DEFAULT NULL,
+  `quantidade9` varchar(100) DEFAULT NULL,
+  `quantidade10` varchar(100) DEFAULT NULL,
+  `quantidade11` varchar(100) DEFAULT NULL,
+  `quantidade12` varchar(100) DEFAULT NULL,
+  `observacao` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `serie`
+--
+
+INSERT INTO `serie` (`mat_usuario`, `exercicio1`, `exercicio2`, `exercicio3`, `exercicio4`, `exercicio5`, `exercicio6`, `exercicio7`, `exercicio8`, `exercicio9`, `exercicio10`, `exercicio11`, `exercicio12`, `repeticao1`, `repeticao2`, `repeticao3`, `repeticao4`, `repeticao5`, `repeticao6`, `repeticao7`, `repeticao8`, `repeticao9`, `repeticao10`, `repeticao11`, `repeticao12`, `quantidade1`, `quantidade2`, `quantidade3`, `quantidade4`, `quantidade5`, `quantidade6`, `quantidade7`, `quantidade8`, `quantidade9`, `quantidade10`, `quantidade11`, `quantidade12`, `observacao`) VALUES
+(5, 'rrr', '', '', '', '', '', '', '', '', '', '', '', '12', '', '', '', '', '', '', '', '', '', '', '', '111', '', '', '', '', '', '', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `treino`
 --
 
 CREATE TABLE `treino` (
@@ -74,17 +128,16 @@ CREATE TABLE `treino` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Fazendo dump de dados para tabela `treino`
+-- Extraindo dados da tabela `treino`
 --
 
 INSERT INTO `treino` (`id`, `id_usuario`, `id_admin`, `data`, `hora`, `descricao`) VALUES
-(4, 5, 12345, '01/01/1970', '9h', 'q'),
-(5, 5, 12345, '01/01/1970', '9h', 'q');
+(6, 5, 12345, '1998-12-12', '10h', 'teste');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuario`
+-- Estrutura da tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -106,7 +159,7 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Fazendo dump de dados para tabela `usuario`
+-- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`matricula`, `nome`, `telefone`, `email`, `endereco`, `senha`, `peso`, `altura`, `cincunferenciaBraco`, `circunferenciaPeito`, `circunferenciaCintura`, `circunferenciaCoxa`, `circunferenciaPanturrilha`, `serie`, `status`) VALUES
@@ -114,45 +167,45 @@ INSERT INTO `usuario` (`matricula`, `nome`, `telefone`, `email`, `endereco`, `se
 (5, 'Rodrigo', '23212321', 'rod@a.com', 'rua b', '827ccb0eea8a706c4c34a16891f84e7b', '0.00', '0.00', '0.00', '0.00', '0.00', '0.00', '0.00', '', 0);
 
 --
--- Índices de tabelas apagadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices de tabela `avaliacao`
+-- Indexes for table `avaliacao`
 --
 ALTER TABLE `avaliacao`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `treino`
+-- Indexes for table `treino`
 --
 ALTER TABLE `treino`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `usuario`
+-- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`matricula`);
 
 --
--- AUTO_INCREMENT de tabelas apagadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `avaliacao`
+-- AUTO_INCREMENT for table `avaliacao`
 --
 ALTER TABLE `avaliacao`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `treino`
+-- AUTO_INCREMENT for table `treino`
 --
 ALTER TABLE `treino`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT de tabela `usuario`
+-- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `matricula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
